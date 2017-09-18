bb452c6a112a0acbf94e6f86fd4f6c71d33ecadf
Myles O'Connor <myles@gumgum.com>
2017-09-18 15:01:11 -0700

    Added boilerplate

---
+++ src/components/PageHeader.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PageHeader extends Component {
+    render() {
+        return (
+            <div>PageHeader</div>
+        );
+    }
+}
+
+PageHeader.propTypes = {
+    //
+};
+
+export default PageHeader;
