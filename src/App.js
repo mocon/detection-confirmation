ead4980d14d50c7966d90c7bbb2f38056f4b0c27
Myles O'Connor <myles@gumgum.com>
2017-09-18 17:52:08 -0700

    Added contextual feedback

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
+            detectionConfirmation: null, // Can be 'yes', 'no', or 'not-sure'
             currentDetection: {
                 imageUrl: 'https://i.ytimg.com/vi/GKZp1jbcg_s/maxresdefault.jpg',
                 detectionGuess: 'Beach'
@@ -21,6 +22,11 @@ class App extends Component {
 
     componentDidMount() {
         window.addEventListener('keydown', this.handleKeyPress);
+    }
+
+    setDetectionConfirmation = (status) => {
+        // Can be 'yes', 'no', or 'not-sure'
+        this.setState({ detectionConfirmation: status });
     }
 
     loadPreviousImage = () => {
@@ -46,6 +52,10 @@ class App extends Component {
     sendCorrectMessage = () => {
         const imageId = 12345; // Replace this with current image ID
 
+        // Update state
+        this.setDetectionConfirmation('yes');
+
+        // Send to API
         Axios.post(`${API_BASE_URL}/confirm/${imageId}`, {
                 confirmation: 'si'
             })
@@ -60,8 +70,30 @@ class App extends Component {
     sendIncorrectMessage = () => {
         const imageId = 12345; // Replace this with current image ID
 
+        // Update state
+        this.setDetectionConfirmation('no');
+
+        // Send to API
         Axios.post(`${API_BASE_URL}/confirm/${imageId}`, {
                 confirmation: 'no'
+            })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    sendNotSureMessage = () => {
+        const imageId = 12345; // Replace this with current image ID
+
+        // Update state
+        this.setDetectionConfirmation('not-sure');
+
+        // Send to API
+        Axios.post(`${API_BASE_URL}/confirm/${imageId}`, {
+                confirmation: 'yo no se'
             })
             .then((response) => {
                 console.log(response);
@@ -82,6 +114,9 @@ class App extends Component {
             case 'y':
                 this.sendCorrectMessage();
                 break;
+            case 'c':
+                this.sendNotSureMessage();
+                break;
             case 'n':
                 this.sendIncorrectMessage();
                 break;
@@ -90,7 +125,21 @@ class App extends Component {
     }
 
     render() {
-        const { currentDetection } = this.state;
+        const { detectionConfirmation, currentDetection } = this.state;
+
+        let confirmationClass = '';
+        switch (detectionConfirmation) {
+            case 'yes':
+                confirmationClass = 'Confirmed--Yes';
+                break;
+            case 'no':
+                confirmationClass = 'Confirmed--No';
+                break;
+            case 'not-sure':
+                confirmationClass = 'Confirmed--NotSure';
+                break;
+            default:
+        }
 
         return (
             <div className="App">
@@ -107,7 +156,9 @@ class App extends Component {
                                 src={ currentDetection.imageUrl }
                                 alt="Processed file"
                             />
-                            <h1 className="gds-text--header-lg -color-tx-lt-4 -block -text-center -m-v-3">{ currentDetection.detectionGuess }</h1>
+                            <h1 className={ `gds-text--header-lg -block -text-center -m-v-3 DetectionText ${confirmationClass}` }>
+                                { currentDetection.detectionGuess }
+                            </h1>
                         </div>
                         <div className="gds-card__block gds-card__block--divide-top gds-flex gds-flex--justify-center">
                             {/* Previous button */}
@@ -123,7 +174,15 @@ class App extends Component {
                                 className="gds-button gds-button--danger -m-b-0 -m-r-2"
                                 onClick={ () => { this.sendIncorrectMessage(); } }
                             >
-                                <div className="gds-hotkey gds-hotkey--white -m-r-2">N</div>Incorrect
+                                <div className="gds-hotkey gds-hotkey--white -m-r-2">N</div>No
+                            </button>
+
+                            {/* Not sure button */}
+                            <button
+                                className="gds-button gds-button--warning -m-b-0 -m-r-2"
+                                onClick={ () => { this.sendNotSureMessage(); } }
+                            >
+                                <div className="gds-hotkey gds-hotkey--white -m-r-2">C</div>Can't Tell
                             </button>
 
                             {/* Correct button */}
@@ -131,7 +190,7 @@ class App extends Component {
                                 className="gds-button gds-button--success -m-b-0 -m-r-2"
                                 onClick={ () => { this.sendCorrectMessage(); } }
                             >
-                                <div className="gds-hotkey gds-hotkey--white -m-r-2">Y</div>Correct
+                                <div className="gds-hotkey gds-hotkey--white -m-r-2">Y</div>Yes
                             </button>
 
                             {/* Next button */}
